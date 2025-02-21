6e66f2e6ba4394f82298efbeae155ef1e3336d2f
curso02 <niidea@niidea.com>
2025-02-21 12:51:13 +0100

    etiquetas del model

--- src/main/java/es/santander/ascender/ejerc005/model/Documento.java
+++ src/main/java/es/santander/ascender/ejerc005/model/Documento.java
@@ -3,12 +3,14 @@ package es.santander.ascender.ejerc005.model;
 import java.time.LocalDate;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Documento {
@@ -17,13 +19,19 @@ public class Documento {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column (name = "nombre",length = 256,unique = true, nullable = false)
+    @Length(max = 256)
+    @Length (min = 1)
+    @NotNull
+    @Column (name = "nombre",unique = true)
     private String nombre;
 
-    @Column (name = "extension", length = 15)
+    @Length(max = 15)
+    @Length(min = 0)
+    @Column (name = "extension")
     private String extension;
 
-    @Column (name = "persona_id", nullable = false)
+    @NotNull
+    @Column (name = "persona_id")
     private Long persona_id;
 
     @CreationTimestamp
